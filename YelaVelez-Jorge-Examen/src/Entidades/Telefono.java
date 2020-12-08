15f1cb698129fdde0d230d0a27e3d9ed23adb76e
jorgeYela <yelajorge6@gmail.com>
2020-12-08 13:52:10 -0600

    Entidad Usuario
    Creando la entidad usuario

---
+++ YelaVelez-Jorge-Examen/src/Entidades/Telefono.java
@@ -0,0 +1,31 @@
+package Entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Telefono
+ *
+ */
+@Entity
+
+public class Telefono implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	private String numero;
+	private String tipo;
+	private String operadora;
+	@ManyToOne
+	@JoinColumn
+	private Usuario usu_tel_id;
+
+	public Telefono() {
+		super();
+	}
+   
+}
--- YelaVelez-Jorge-Examen/src/Entidades/Usuario.java
+++ YelaVelez-Jorge-Examen/src/Entidades/Usuario.java
@@ -1,8 +1,8 @@
 package Entidades;
 
-import java.io.Serializable;
+import java.io.Serializable; 
 import javax.persistence.*;
-
+import java.util.List;
 /**
  * Entity implementation class for Entity: Usuario
  *
@@ -15,10 +15,80 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int codigo;
+	private String nombre;
+	private String apellido;
+	private String cedula;
+	private String correo;
+	private String contrasena;
+	@OneToMany(cascade = CascadeType.ALL ,mappedBy = "usu_tel_id")
+	private List<Telefono> telefonos;
 
 	public Usuario() {
 		super();
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public List<Telefono> getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(List<Telefono> telefonos) {
+		this.telefonos = telefonos;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [codigo=" + codigo + ", nombre=" + nombre + ", apellido=" + apellido + ", cedula=" + cedula
+				+ ", correo=" + correo + ", contrasena=" + contrasena + ", telefonos=" + telefonos + "]";
 	}
    
 }
