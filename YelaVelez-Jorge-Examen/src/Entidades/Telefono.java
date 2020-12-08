0c41fcbe3afc227500957a4e8e7ad39aeabce13f
jorgeYela <yelajorge6@gmail.com>
2020-12-08 13:54:25 -0600

    Entidd Telefono
    Creando entidad telefono

--- YelaVelez-Jorge-Examen/src/Entidades/Telefono.java
+++ YelaVelez-Jorge-Examen/src/Entidades/Telefono.java
@@ -27,5 +27,51 @@ public class Telefono implements Serializable {
 	public Telefono() {
 		super();
 	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getOperadora() {
+		return operadora;
+	}
+
+	public void setOperadora(String operadora) {
+		this.operadora = operadora;
+	}
+
+	public Usuario getUsu_tel_id() {
+		return usu_tel_id;
+	}
+
+	public void setUsu_tel_id(Usuario usu_tel_id) {
+		this.usu_tel_id = usu_tel_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Telefono [codigo=" + codigo + ", numero=" + numero + ", tipo=" + tipo + ", operadora=" + operadora
+				+ ", usu_tel_id=" + usu_tel_id + "]";
+	}
    
 }
